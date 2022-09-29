8d45f91b6c8704d12de5c3c23157bd4e2330ef6d
Dov Davidovics <dov@medadoc.com>
2022-09-29 15:46:09 -0700

    redirect stdout

--- uwsgi/foobar.py
+++ uwsgi/foobar.py
@@ -1,3 +1,7 @@
+
+import sys
+sys.stdout = sys.stderr
 def application(env, start_response):
+    print ("in uwsgi applicatiom ------------------------------------------")
     start_response('200 OK', [('Content-Type','text/html')])
     return [b"Hello World"]
