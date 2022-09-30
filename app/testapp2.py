83a9af4e130b4a90042992020dbb75ec4cae330b
Dov Davidovics <dov@medadoc.com>
2022-09-30 21:30:07 +0000

    test change in git

--- app/testapp2.py
+++ app/testapp2.py
@@ -1,6 +1,7 @@
 from wsgiref.simple_server import make_server
 
 def application(environ, start_response):
+    # test
     print ("in application")
     start_response("200 OK", [("Content-type", "text/plain")])
     return ["Hello my friend!".encode("utf-8")]
