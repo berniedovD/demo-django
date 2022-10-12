6a9c5689e9062e126f705da3780bee8a117b8765
Dov Davidovics <dov@medadoc.com>
2022-10-12 08:37:33 -0700

    change mysql db settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'defaultsql': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appdb',
+        'USER': 'appdbuser',
+        'PASSWORD': 'tortola1',
+        'HOST': '10.20.11.252',
+        'PORT': '3306'
     }
 }
 
