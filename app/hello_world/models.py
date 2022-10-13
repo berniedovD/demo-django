e311f446f1a257707de59898f5bd9b14a3607efe
Dov Davidovics <dov@medadoc.com>
2022-10-13 14:57:20 -0700

    add database to demo django hello world

---
+++ app/hello_world/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Puzzle (models.Model):
+    TYPE_CHOICES = [
+        ('E', 'Easy'),
+        ('M', 'Medium'),
+        ('H', 'Hard')]
+    
+    puzzleID = models.CharField(max_length=4, primary_key=True)
+    puzzleName = models.CharField(max_length=10)
+    sudukoString = models.CharField(max_length=81)
+    difficultyLevel = models.CharField(max_length=1, choices=TYPE_CHOICES)
+    newfield = models.CharField(max_length=5)
+    
