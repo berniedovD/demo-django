e6f8fd6e061dadf580098589ac6ecaff1936ccc1
Dov Davidovics <dov@medadoc.com>
2022-10-13 12:57:17 -0700

    put back mysql config

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-
+#DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 if ALLOWED_HOSTS_ENV:
@@ -76,18 +76,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
-    'defaultsql': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-        'default': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'appdb',
         'USER': 'appdbuser',
         'PASSWORD': 'tortola1',
-        'HOST': '10.20.11.252',
-        'PORT': '3306'
+        'HOST': '52.87.211.153',
+        'PORT': '3306',
     }
 }
 
